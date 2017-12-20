4d17016cab7de0d78193dffd30ce26eef2160a27
Benjamin Blott <benjaminblott@verticalonellc.com>
2017-12-20 14:34:51 -0600

    all stats; see note
    create all stats at beginning of data frame, build z-scores for the population of each stat, then let user decide which ones they want for their own league

--- 2017_mlb_projections_eoy_7.R
+++ 2017_mlb_projections_eoy_7.R
@@ -40,7 +40,9 @@ starting_pitchers <- 7
 relief_pitchers <- 3
 
 #user selected stats
-#i.e. avg, runs, hr, rbi, ops, sb_net
+  #from fangraphs:
+#avg, rbi, r, sb, hr, obp, slg, ops, h, so, 2b, 3b, tb, bb, rbi+r, xBH, sb-cs, woba
+#w, sv, era, whip, so, avg, k/9, bb/9, k/bb, fip, ip, hr, hld, qs
 
 #staring players per position for a fantasy league
 n_catchers <- n_teams * starting_catchers
@@ -107,7 +109,7 @@ z_score_sp <- function(df, n_df) {
 z_score_all_p <- function(df, n_df) {
   df <- df %>%
     filter(games > 1) %>%  #many players have a default of "1" game played for "just in case they get moved to the majors" and other scenarios
-#           gs > 0) %>%  #filter for pitchers who project to start at least one game
+    #           gs > 0) %>%  #filter for pitchers who project to start at least one game
     select(name, team, gs, games, ip, wins, era, saves, hra, so, whip)
   
   df$wins_z <- as.numeric(z_score(df$wins))
@@ -177,8 +179,8 @@ relievers1 <- z_score_rp(relievers, n_relief_pitchers)
 #NOTES
 #a one unit change in a reliever counting stat category should be the same as a one unit change in a starter counting stat cat.
 #likewise, there should be a regulator for rate stats base on ip or pa.
-  #So! maybe need to change everthing into weights or everything into counting stats
-    #What would W/IP look like? No, each win should be worth a certain amount of Z.
+#So! maybe need to change everthing into weights or everything into counting stats
+#What would W/IP look like? No, each win should be worth a certain amount of Z.
 
 #probably first need a straight scale. then have a second statistic that describes how it affects team outcome
 #this second stat will be weighted with innings
@@ -230,52 +232,61 @@ hitters_new <- catchers %>%
   full_join(second_basemen) %>%
   full_join(third_basemen) %>%
   full_join(shortstops) %>%
-  full_join(outfielders)
+  full_join(outfielders) %>%
+  filter(pa > 1)  #get this done out of the gate
 
 #start searching for multi-position players
 hitters_names <- as.data.frame(hitters_new$name)  #creates a single col df of all starters' names
 names(hitters_names) <- "name"  #names the col in above df
-hitters_names <- hitters_names %>%  #arrange alphabetically so you can look for duplicates
-  arrange(name)
+  #this next command is just here for visualization
+#hitters_names <- hitters_names %>%  #arrange alphabetically so you can look for duplicates
+#  arrange(name)
 
-duplicated_names <- as.data.frame(hitters_names[duplicated(hitters_names),])  #creates a df of duplicated names
+#creates an Nx1 df of duplicated names
+duplicated_names <- as.data.frame(hitters_names[duplicated(hitters_names),])
 names(duplicated_names) <- "name"  #names the col in above df
 
+#creates full duplicated names df
 duplicated_names1 <- hitters_new %>%
   right_join(duplicated_names, by = "name")
-duplicated_names_copy <- duplicated_names1  #save this for anti_join with full hitters
+#make a copy of this for anti_join with full hitters
+duplicated_names_copy <- duplicated_names1
 duplicated_names1$pos <- as.numeric(duplicated_names1$pos)
 
 duplicated_names1 <- unique(duplicated_names1)  #this removes rows where the name AND position are duplicated
 
-#create vector of position rankings, with the least productive position, catcher, having the most value
+#create vector of position rankings, with the least productive position, catcher, having the most relative value
+  #and assiging the most valuable position to each duplicate player
+    #call that new var "pos_rank"
 duplicated_names1$pos_rank <- ifelse(duplicated_names1$pos == 2, 1,
                                      ifelse(duplicated_names1$pos == 6, 2, 
                                             ifelse(duplicated_names1$pos == 4, 3,
                                                    ifelse(duplicated_names1$pos == 7, 4,
                                                           ifelse(duplicated_names1$pos == 3, 5, 6)))))
-
+#assign pos_rank to each duplicate player
 duplicated_names2 <- duplicated_names1 %>%
   select(name, pos_rank) %>%
   group_by(name) %>%
   summarize(pos_rank = min(pos_rank))
-
+#join the position rank df with the now singular duplicated player df
 duplicated_names3 <- duplicated_names1 %>%
   right_join(duplicated_names2) %>%
-  select(-pos_rank)
+  select(-pos_rank)  #remove the no longer needed pos_rank var
 duplicated_names3$pos <- as.character(duplicated_names3$pos)  #switch back to char class for joining with rest of hitters
-
+#combine dfs so there is only one line for each player
+  #each player has the most valuable position
 hitters_new <- hitters_new %>%
   anti_join(duplicated_names_copy) %>%
   bind_rows(duplicated_names3) %>%
-  filter(pa > 1) %>%
+  mutate(tb = hit + double + 2 * triple + 3 * hr,  #total bases
+         rbi_r = rbi + runs, #rbis plus runs
+         xbh = double + triple + hr,  #extra base hits
+         sb_net = sb - cs) %>%
   arrange(name)
 
-#duplicated_names4 <- duplicated_names3[duplicated(duplicated_names3),]
-#duplicated_names5 <- duplicated_names3 %>%
-#  anti_join(duplicated_names4) %>%
-#  full_join(duplicated_names4)
-###
+#build stat projections for all missing stats, i.e. sb_net
+#avg, rbi, r, sb, hr, obp, slg, ops, h, so, 2b, 3b, tb, bb, rbi+r, xBH, sb-cs, woba
+  #tb, rbi+r, xbh, sb-cs (these are the ones that need mutating)
 
 catchers1 <- hitters_new %>% filter(pos == "2")
 first_basemen1 <- hitters_new %>% filter(pos == "3")
@@ -285,25 +296,13 @@ shortstops1 <- hitters_new %>% filter(pos == "6")
 outfielders1 <- hitters_new %>% filter(pos == "7")
 
 #run sb_net fn for each position
-catchers1$sb_net <- sb_net_fn(catchers1$sb, catchers1$cs)
-first_basemen1$sb_net <- sb_net_fn(first_basemen1$sb, first_basemen1$cs)
-second_basemen1$sb_net <- sb_net_fn(second_basemen1$sb, second_basemen1$cs)
-third_basemen1$sb_net <- sb_net_fn(third_basemen1$sb, third_basemen1$cs)
-shortstops1$sb_net <- sb_net_fn(shortstops1$sb, shortstops1$cs)
-outfielders1$sb_net <- sb_net_fn(outfielders1$sb, outfielders1$cs)
-hitters_new$sb_net <- sb_net_fn(hitters_new$sb, hitters_new$cs)
-
-#remove dual position players from "weaker" position category; i.e. remove Starlin Castro from "2B" and leave in "SS"
-#first_basemen <- first_basemen %>%
-#  filter(name != "Brandon Moss", name != "Steve Pearce")
-#second_basemen <- second_basemen %>%
-#  filter(name != "Brad Miller", name != "Jean Segura", name != "Starlin Castro", name != "Jonathan Villar", name != "Javier Baez")
-#third_basemen <- third_basemen %>%
-#  filter(name != "Jedd Gyorko", name != "Jose Ramirez", name != "Matt Carpenter", name != "Yangervis Solarte")
-#outfielders <- outfielders %>%
-#  filter(name != "Ben Zobrist", name != "Ian Desmond", name != "Howie Kendrick")
-
-
+# catchers1$sb_net <- sb_net_fn(catchers1$sb, catchers1$cs)
+# first_basemen1$sb_net <- sb_net_fn(first_basemen1$sb, first_basemen1$cs)
+# second_basemen1$sb_net <- sb_net_fn(second_basemen1$sb, second_basemen1$cs)
+# third_basemen1$sb_net <- sb_net_fn(third_basemen1$sb, third_basemen1$cs)
+# shortstops1$sb_net <- sb_net_fn(shortstops1$sb, shortstops1$cs)
+# outfielders1$sb_net <- sb_net_fn(outfielders1$sb, outfielders1$cs)
+# hitters_new$sb_net <- sb_net_fn(hitters_new$sb, hitters_new$cs)
 
 
 #run z-score pos on each position df

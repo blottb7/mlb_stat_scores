8d426ee63e65c6240212df1b1dce76c858d803cc
Benjamin Blott <benjaminblott@verticalonellc.com>
2018-01-17 22:48:41 +0000

    vertical layout

--- 2017_mlb_projections_eoy_7.R
+++ 2017_mlb_projections_eoy_7.R
@@ -4,61 +4,62 @@ library(shiny)
 
 #ui
 ui <- fluidPage(
-  #layout
-  _____Layout
   
-  #SELECT INPUT
-  
-  #number of teams
-  sliderInput(
-    inputId = n_teams, label = "Number of Fantasy Teams", min = 8, max = 24, selected = "12", step = 1
-  )
-  
-  #starting catchers per team
-  numericInput(
-    inputId = starting_catchers, label = "starting catchers per team", value = 1, min = 0, max = 3, step = 1
+  #LAYOUT
+  verticalLayout(
+    
+    #SELECT INPUT
+    #number of teams
+    sliderInput(
+      inputId = n_teams, label = "Number of Fantasy Teams", min = 8, max = 24, selected = "12", step = 1
     )
-  
-  #starting first basemen per team
-  numericInput(
-    inputId = starting_first_basemen, label = "starting first basemen per team", value = 1, min = 0, max = 3, step = 1
+    
+    #starting catchers per team
+    numericInput(
+      inputId = starting_catchers, label = "starting catchers per team", value = 1, min = 0, max = 3, step = 1
+    )
+    
+    #starting first basemen per team
+    numericInput(
+      inputId = starting_first_basemen, label = "starting first basemen per team", value = 1, min = 0, max = 3, step = 1
+    )
+    
+    #starting second basemen per team
+    numericInput(
+      inputId = starting_second_basemen, label = "starting second basemen per team", value = 1, min = 0, max = 3, step = 1
+    )
+    
+    #starting third basemen per team
+    numericInput(
+      inputId = starting_third_basemen, label = "starting third basemen per team", value = 1, min = 0, max = 3, step = 1
+    )
+    
+    #starting shortstops per team
+    numericInput(
+      inputId = starting_shortstops, label = "starting shortstops per team", value = 1, min = 0, max = 3, step = 1
+    )
+    
+    #starting outfielders per team
+    numericInput(
+      inputId = starting_outfielders, label = "starting outfielders per team", value = 5, min = 0, max = 9, step = 1
+    )
+    
+    #starting middle infielders per team
+    numericInput(
+      inputId = starting_middle_infielders, label = "starting middle infielders per team", value = 1, min = 0, max = 3, step = 1
+    )
+    
+    #starting corner infielders per team
+    numericInput(
+      inputId = starting_corner_infielders, label = "starting corner infielders per team", value = 1, min = 0, max = 3, step = 1
+    )
+    
+    #starting designated hitters per team
+    numericInput(
+      inputId = starting_designated_hitters, label = "starting designated hitters/utilty per team", value = 1, min = 0, max = 3, step = 1
+    )
+    
   )
-  
-  #starting second basemen per team
-  numericInput(
-    inputId = starting_second_basemen, label = "starting second basemen per team", value = 1, min = 0, max = 3, step = 1
-  )
-  
-  #starting third basemen per team
-  numericInput(
-    inputId = starting_third_basemen, label = "starting third basemen per team", value = 1, min = 0, max = 3, step = 1
-  )
-  
-  #starting shortstops per team
-  numericInput(
-    inputId = starting_shortstops, label = "starting shortstops per team", value = 1, min = 0, max = 3, step = 1
-  )
-  
-  #starting outfielders per team
-  numericInput(
-    inputId = starting_outfielders, label = "starting outfielders per team", value = 5, min = 0, max = 9, step = 1
-  )
-  
-  #starting middle infielders per team
-  numericInput(
-    inputId = starting_middle_infielders, label = "starting middle infielders per team", value = 1, min = 0, max = 3, step = 1
-  )
-  
-  #starting corner infielders per team
-  numericInput(
-    inputId = starting_corner_infielders, label = "starting corner infielders per team", value = 1, min = 0, max = 3, step = 1
-  )
-  
-  #starting designated hitters per team
-  numericInput(
-    inputId = starting_designated_hitters, label = "starting designated hitters/utilty per team", value = 1, min = 0, max = 3, step = 1
-  )
-  
   #MAIN PANEL
   mainPanel()
 )
